bbc258851442fb29ed3fd49a5767ae149a1d30df
Cathrinesj <cathrine.juvsett@gmail.com>
2021-12-19 21:25:58 +0100

    added back to top bloglist + fixes

--- js/posts.js
+++ js/posts.js
@@ -30,30 +30,6 @@ async function fetchPost() {
 
 fetchPost();
 
-//Icons
-
-topIcon = document.querySelector(".fa-caret-square-up");
-shareIcon = document.querySelector(".fa-share-alt-square");
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    topIcon.style.display = "block";
-    shareIcon.style.display = "block";
-  } else {
-    topIcon.style.display = "none";
-    shareIcon.style.display = "none";
-  }
-}
-
-function toTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 scrollFunction();
 
 //Image modal
--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ function hamburgerNav() {
     hamburgerMenu.style.display = "block";
   }
 }
+
+//Icons
+
+topIcon = document.querySelector(".fa-caret-square-up");
+shareIcon = document.querySelector(".fa-share-alt-square");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topIcon.style.display = "block";
+    shareIcon.style.display = "block";
+  } else {
+    topIcon.style.display = "none";
+    shareIcon.style.display = "none";
+  }
+}
+
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
